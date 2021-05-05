25089cac2c6ec863fa49ca2933b85c1fd06f602b
filipe-m-cunha <filipe.miranda.cunha@tecnico.ulisboa.pt>
2021-05-05 13:15:42 +0100

    Added Import Possibility from .csv files

--- Proj/projectoEntrega1/src/projectoEntrega1/Dataset.java
+++ Proj/projectoEntrega1/src/projectoEntrega1/Dataset.java
@@ -1,41 +1,72 @@
 package projectoEntrega1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Dataset {
 	
 	int dim;
 	ArrayList<int[]> values;
 	int domain[];
-	int size;
 	
 	
 	
 	public Dataset(int dim) {
-		this.dim = dim;
+		super();
+		this.dim = 0;
 		this.values = new ArrayList<int[]>();
-		this.domain = new int[dim];
-		for(int i=0; i<dim; i++) {
-			domain[i] = 0;
+		this.domain = null;
+	}
+	
+	public Dataset(String file) throws IOException {
+		
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line;
+		while((line=reader.readLine())!= null) {
+			System.out.println(line);
 		}
-		this.size=0;
+		reader.close();
 	}
 	
 	public void Add(int[] vector) throws Exception{
-		if(vector.length == this.dim) {
+		if(this.dim == 0 ) {
 			this.values.add(vector);
-			this.size += 1;
-			for(int i = 0; i<dim; i++) {
-				if(vector[i]>this.domain[i]) {
-					this.domain[i] = vector[i];
+			this.dim = vector.length;
+			this.domain = vector;
+			}
+		else{
+			if(vector.length == this.dim) {
+				this.values.add(vector);
+				for(int i = 0; i<dim; i++) {
+					if(vector[i]>this.domain[i]) {
+						this.domain[i] = vector[i];
+					}
 				}
 			}
-		}
-		else {
-			throw new Exception("vector size does not match dataset size");
+			else {
+				throw new Exception("vector size does not match dataset size");
+			}
 		}
 	}
 	
+	public String toStringaux() {
+		String s= "";
+		for (int[] a:values) {
+			s = s+"\n" + Arrays.toString(a);
+		}
+		return s;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Dataset [dim=" + dim + ", values=" + toStringaux() + ", domain=" + Arrays.toString(domain) + "]";
+	}
+
 	public int Count(ArrayList<Integer> indices, ArrayList<Integer> vector) {
 		int count = 0;
 		for(int i = 0; i<values.size(); i++) {
